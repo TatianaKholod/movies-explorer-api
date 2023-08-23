5535df9838b381952f6afd9b168d2982dafe4642
Tatiana Kholod <tkholod@yandex.ru>
2023-08-23 22:26:21 +0300

    что-то с кукой

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,6 +23,7 @@ const login = (req, res, next) => {
       res.cookie('token', token, {
         maxAge: +COOKIE_MAXAGE,
         httpOnly: true,
+        sameSite: 'None',
       });
       const u = data.toJSON();
       delete u.password;
